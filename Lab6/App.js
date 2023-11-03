348d1e1cd195f4f3c2bcb0adad2fc4d0bae246d2
vncharles <quoctrong.13022002@gmail.com>
2023-11-03 16:47:35 +0700

    todo list

--- Lab6/App.js
+++ Lab6/App.js
@@ -5,12 +5,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 import Home from "./screens/Home";
+import TodoList from "./screens/TodoList";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+      <Stack.Navigator initialRouteName="TodoList" screenOptions={{headerShown:false}}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="TodoList" component={TodoList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Lab6/screens/TodoList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TodoList = ({navigation}) => {
+  return (
+    <View>
+      <Text>TodoList</Text>
+    </View>
+  )
+}
+
+export default TodoList
+
+const styles = StyleSheet.create({})
